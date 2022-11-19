3d8ece2af9a38649daec32413084f59045cc18de
Ahmed Mubarak <ahmed.mohammed2@vodafone.com>
2022-11-19 23:03:22 +0200

    filesystem doc added

--- src/server/runLocalServer.ts
+++ src/server/runLocalServer.ts
@@ -2,6 +2,16 @@ import express from 'express';
 import routes from './routes/api';
 const app = express();
 const port = 5000;
+const logger = (
+  req: express.Request,
+  res: express.Response,
+  next: Function
+) => {
+  let url = req.url;
+  console.log('${url}');
+  next();
+};
+app.use(logger);
 app.use('/api', routes);
 // app.get('/api', (req, res) => {
 //   res.send('Hello, World');
